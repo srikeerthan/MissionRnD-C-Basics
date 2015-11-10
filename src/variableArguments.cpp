38522c0cd73a9bb32ab81aaa2a53b3143a15b9b5
srikeerthan <srikeerthanreddy@gmail.com>
2015-11-10 17:29:54 +0530

    Submit Commit

--- src/variableArguments.cpp
+++ src/variableArguments.cpp
@@ -17,5 +17,15 @@ NOTES: 		use stdarg.h header.
 
 int variableArguments(int arg_count, ...)
 {
-	return 0;
+	int i,n,count=0;
+	va_list ap;
+	va_start(ap, arg_count);
+	for (i = 0; i < arg_count; i++)
+	{
+		n=va_arg(ap,int);
+		if (n>90)
+			count++;
+	}
+	va_end(ap);
+	return count;
 }
